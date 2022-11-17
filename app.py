ee6a9487aa3ce87a42172fb9758fb1dfec08a476
nayan gurung <nayangrg.uni@gmail.com>
2022-11-17 11:52:06 +0000

    second commit

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 from flask import Flask, render_template
+from flask import render_template # Remove: import Flask
+import connexion
+
+app = connexion.App(__name__, specification_dir="./")
+app.add_api("swagger.yml")
 
 app = Flask(__name__)
 
